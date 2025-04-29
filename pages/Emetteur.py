d88596580731ba1e5d52816ea9967697fb411808
MohammedIB <88784764+MohammedIB@users.noreply.github.com>
2025-04-29 14:06:02 +0100

    Create Emetteur.py

---
+++ pages/Emetteur.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 27 19:07:34 2025
+
+@author: ibrah
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.markdown("# Emetteur")
+st.sidebar.markdown("# Emetteur")
